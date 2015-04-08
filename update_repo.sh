e975a9b7eb3d38b1704de3d64aa51ffb1d04c223
msp132 <msp132@comcast.net>
2015-04-08 05:46:19 -0400

    update

--- update_repo.sh
+++ update_repo.sh
@@ -3,7 +3,8 @@ cd ..
 mkdir temp
 cd temp
 git clone https://github.com/msp132/maven-repo.git
-cp ../sample-jar/build/libs/sample-jar-1.jar .
+cp ../sample-jar/build/libs/sample-jar-1.jar maven-repo
+cd maven-repo
 git add *
 git commit -m -a "new version"
 ls -al
